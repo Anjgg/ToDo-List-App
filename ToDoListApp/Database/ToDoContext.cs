d864d9a162d6b3195f1d885a82483d3d2ff8d8ff
Julien LEROY <julien.leroy.work@gmail.com>
2024-12-06 18:09:45 +0100

    Add project files.

---
+++ ToDoListApp/Database/ToDoContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.SqlClient;
+using ToDoListApp.Models;
+
+
+namespace ToDoListApp.Database
+{
+    public class ToDoContext : DbContext
+    {
+        private IDbConnection DbConnection { get; }
+
+        public ToDoContext(DbContextOptions<ToDoContext> options, IConfiguration configuration) : base(options)
+        {
+            DbConnection = new SqlConnection(configuration.GetConnectionString("P3Referential"));
+        }
+
+        public DbSet<ToDoModel> ToDoList { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DbConnection.ConnectionString, providerOptions => providerOptions.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.1-servicing-10028");
+
+            modelBuilder.Entity<OrderLine>(entity =>
+            {
+                entity.HasIndex(e => e.OrderId)
+                    .HasName("IX_OrderLineEntity_OrderEntityId");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderLine)
+                    .HasForeignKey(d => d.OrderId)
+                    .HasConstraintName("FK_OrderLineEntity_OrderEntity_OrderEntityId").OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderLine)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__OrderLine__Produ__52593CB8").OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+}
---
+++ ToDoListApp/Models/ToDoModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApp.Models
+{
+    public class ToDoModel
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public bool Urgent { get; set; }
+    }
+}
